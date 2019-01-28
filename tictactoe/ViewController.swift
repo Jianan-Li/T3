ab14b69fc2d0e20735773f7376940311e8ab0c1f
Jianan Li <ljn07050@gmail.com>
2019-01-28 10:01:15 -0500

    Initial Commit

---
+++ tictactoe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tictactoe
+//
+//  Created by Jianan Li on 1/28/19.
+//  Copyright © 2019 Jianan Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
